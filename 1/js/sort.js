cd72b2990c094d3da94db4df1cd38721ce01bc83
LiJohnson <598420668@qq.com>
2013-03-24 03:33:34 +0000

    git-svn-id: https://svn.sinacloud.com/gtbcode@42 5f1940d7-40b4-418f-bb40-7c606c48f36d

---
+++ 1/js/sort.js
@@ -0,0 +1,12 @@
+//sort
+;(function(){
+	var defaultCompareor = function( a , b ){ return a - b; };
+	var exported = {};
+	exported.swap = function(n,m){
+		var tmp = this[n];
+		this[n] = this[m];
+		this[m] = tmp;
+	};
+	
+	for( var k in exported  ){ Array.prototype[k]=exported[k]; }
+})();
